50da2008ad1054bce8170efd3737267ede2fb8b9
juanpa0011 <juanpablo1996epifani@gmail.com>
2022-02-15 07:27:47 -0300

    init chapter on

--- renderer.js
+++ renderer.js
@@ -1,39 +1,28 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+const newGame = document.getElementById("newgame");
 
-var $menu = $('.Menu-list'),
-    $item = $('.Menu-list-item'),
-    w = $(window).width(), //window width
-    h = $(window).height(); //window height
+const starFallIntro = document.getElementsByClassName("falling-stars")[0]
 
-$(window).on('mousemove', function(e) {
-  var offsetX = 0.5 - e.pageX / w, //cursor position X
-      offsetY = 0.5 - e.pageY / h, //cursor position Y
-      dy = e.pageY - h / 2, //@h/2 = center of poster
-      dx = e.pageX - w / 2, //@w/2 = center of poster
-      theta = Math.atan2(dy, dx), //angle between cursor and center of poster in RAD
-      angle = theta * 180 / Math.PI - 90, //convert rad in degrees
-      offsetPoster = $menu.data('offset'),
-      transformPoster = 'translate3d(0, ' + -offsetX * offsetPoster + 'px, 0) rotateX(' + (-offsetY * offsetPoster) + 'deg) rotateY(' + (offsetX * (offsetPoster * 2)) + 'deg)'; //poster transform
+const mainMenu = document.getElementsByClassName("Menu-list")[0]
+const IntroContainer = document.getElementsByClassName("Background--menu")[0]
 
-  //get angle between 0-360
-  if (angle < 0) {
-    angle = angle + 360;
-  }
+const chap1 = document.getElementById("chapter-one");
 
-  //poster transform
-  $menu.css('transform', transformPoster);
 
-  //parallax for each layer
-  $item.each(function() {
-    var $this = $(this),
-        offsetLayer = $this.data('offset') || 0,
-        transformLayer = 'translate3d(' + offsetX * offsetLayer + 'px, ' + offsetY * offsetLayer + 'px, 20px)';
+newGame.addEventListener("click", () => {
+    fadeOut(mainMenu);
+    fadeOut(starFallIntro);
+    fadeIn(chap1);
+}) 
 
-    $this.css('transform', transformLayer);
-  });
-});
+ function fadeOut(element) {
+    element.style.opacity = 0;
+    setTimeout(() => {
+      element.classList.add("hidden")
+    }, 2000);
+}
+function fadeIn(element) {
+  element.style.opacity = 1;
+  setTimeout(() => {
+    element.classList.remove("hidden")
+  }, 2000);
+}
